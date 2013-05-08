d1cc814d12f5f74f1452498f406fc6a4f4e92490
Ehsan Ullah <eullah01@gmail.com>
2013-05-08 17:07:07 -0400

    Added comments to GenerateCombos and EFMGenerator

--- gpu-source/GenerateCombos.h
+++ gpu-source/GenerateCombos.h
@@ -3,6 +3,12 @@
 
 #include "Setup.h"
 
+//This function generates combinations after dependency checks have identifiied independent
+//input output pairs. The process of generation includes generating binary bit vectors and
+//metbaolite coefficeints of the combinations.
+//Param metabolite: The metabolite being balanced
+//Param numberOfBin: Number of bins in combination bin array
+//Param nextFreePathwayIndex: Index in the pathway buffers to store new pathways
 int generateCombinations(int metabolite, int numberOfBins, int nextFreePathwayIndex);
 
 #endif
